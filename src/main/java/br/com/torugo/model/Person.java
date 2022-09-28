230ed84ac35b87386b1afbdb553172556fd02463
Vhmarega <victorhugomarega@hotmail.com>
2022-09-28 19:36:33 -0300

    Revert "6 - Refactoring, trocando getters e setters por @Getter e @Setter usando Lombok"
    This reverts commit 8f3d182fa2654c8ebc50a9a38f806f8b931eb08b.

--- src/main/java/br/com/torugo/model/Person.java
+++ src/main/java/br/com/torugo/model/Person.java
@@ -2,8 +2,6 @@ package br.com.torugo.model;
 
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.*;
@@ -11,8 +9,6 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "person")
-@Getter
-@Setter
 public class Person extends PanacheEntity {
 
 
@@ -34,4 +30,43 @@ public class Person extends PanacheEntity {
     @PrimaryKeyJoinColumn
     private College college=null;
 
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public College getCollege() {
+        return college;
+    }
+
+    public void setCollege(College college) {
+        this.college = college;
+    }
 }
