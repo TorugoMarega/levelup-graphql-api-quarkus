8e782dc1de08289c533eb1402ac6a8a9c695853e
Vhmarega <victorhugomarega@hotmail.com>
2022-09-28 19:39:33 -0300

    6 - Refactoring, implementando getters e setters do Lombok

--- src/main/java/br/com/torugo/model/Address.java
+++ src/main/java/br/com/torugo/model/Address.java
@@ -1,24 +1,12 @@
 package br.com.torugo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Address {
     private String street;
     private Integer number;
 
-
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
 }
--- src/main/java/br/com/torugo/model/Person.java
+++ src/main/java/br/com/torugo/model/Person.java
@@ -2,6 +2,8 @@ package br.com.torugo.model;
 
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.*;
@@ -9,6 +11,8 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "person")
+@Getter
+@Setter
 public class Person extends PanacheEntity {
 
 
@@ -29,44 +33,4 @@ public class Person extends PanacheEntity {
     @MapsId
     @PrimaryKeyJoinColumn
     private College college=null;
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public College getCollege() {
-        return college;
-    }
-
-    public void setCollege(College college) {
-        this.college = college;
-    }
 }
