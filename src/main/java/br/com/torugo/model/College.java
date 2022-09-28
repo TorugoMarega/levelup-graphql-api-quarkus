8f3d182fa2654c8ebc50a9a38f806f8b931eb08b
Vhmarega <victorhugomarega@hotmail.com>
2022-09-28 19:34:44 -0300

    6 - Refactoring, trocando getters e setters por @Getter e @Setter usando Lombok

--- src/main/java/br/com/torugo/model/College.java
+++ src/main/java/br/com/torugo/model/College.java
@@ -1,39 +1,20 @@
 package br.com.torugo.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "college")
+@Getter
+@Setter
 public class College extends PanacheEntity {
 
     private String course;
     private String university;
     private String period;
 
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
-    }
-
-    public String getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(String period) {
-        this.period = period;
-    }
 }
