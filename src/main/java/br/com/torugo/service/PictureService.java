d8c12a01c777bbf505a391d85b8d491b4040181e
Vhmarega <victorhugomarega@hotmail.com>
2022-09-29 19:47:05 -0300

    8 - Correção no Picture Service

--- src/main/java/br/com/torugo/service/PictureService.java
+++ src/main/java/br/com/torugo/service/PictureService.java
@@ -2,8 +2,9 @@ package br.com.torugo.service;
 
 import br.com.torugo.model.User;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
-
+@ApplicationScoped
 public class PictureService {
     //--------------------------------- ATUALIZA FOTOS DE PERFIL E AVATAR --------------------------------
     @Transactional
